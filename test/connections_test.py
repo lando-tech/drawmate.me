9678187a24480f0eea655570a1ea1ca7feb89757
Aaron <157839802+lando-tech@users.noreply.github.com>
2024-09-08 11:06:32 +0900

    updates

---
+++ test/connections_test.py
@@ -0,0 +1,15 @@
+import json
+import re
+
+
+path_var = '/home/landotech/Documents/GitHub/drawmate.me/data/json_files/VideoNew-2024-09-06 13:21:08.825787.json'
+pattern1 = r'\{\{.*?\}\}'
+
+av_connections = {}
+
+with open(path_var, 'r', encoding='utf-8') as new_connection:
+    data = json.load(new_connection)
+    
+    for keys, values in data["diagram"]["mxGraphModel"]["root"].items():
+        for value in values:
+            print(value)
